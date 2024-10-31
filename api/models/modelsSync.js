938898127d629bd79a28dc760475d1c6780626d2
Princess585 <princessdkhombisa1994@gmail.com>
2024-10-31 10:13:33 +0200

    Update modelsSync.js

--- api/models/modelsSync.js
+++ api/models/modelsSync.js
@@ -4,10 +4,8 @@ require("./relationship");
 (async () => {
     try {
         await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
 
         await sequelize.sync({ alter: true });
-        console.log("All models and tables are synchronized successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
